3a5a07480f452148e43cef1f57c493cf76643b9e
migariane <watzilei@hotmail.com>
2023-05-15 09:00:14 +0200

    update

--- QuantilesDistProbaCont.R
+++ QuantilesDistProbaCont.R
@@ -1,4 +1,4 @@
-# QUANTILES DISTRIBUCION NORMAL
+# CUANTILES DISTRIBUCION NORMAL
 qnorm(c(0.975), mean=0, sd=1, lower.tail=TRUE)
 # NC 90
 qnorm(0.95)
@@ -13,16 +13,15 @@ qnorm(0.995)
 1012 + qnorm(0.975)*(25/sqrt(64))
 1012 - qnorm(0.975)*(25/sqrt(64))
 
-# QUANTILES DISTRIBUCION T-STUDENT 
+# CUANTILES DISTRIBUCION T-STUDENT 
 qt(c(0.975), df=8, lower.tail=TRUE)
 
-
-# QUANTILES DISTRIBUCION CHI-CUADRADO
+# CUANTILES DISTRIBUCION CHI-CUADRADO
 qchisq(c(0.9), df=15, lower.tail=TRUE)
 qchisq(c(0.1), df=15, lower.tail=TRUE)
 qchisq(c(0.9,0.1), df=15, lower.tail=TRUE)
 
-# QUANTILES DISTRIBUCION F SNEDECOR 
+# CUANTILES DISTRIBUCION F SNEDECOR 
 qf(c(0.975), df1=24, df2=24, lower.tail=TRUE)
 
 
