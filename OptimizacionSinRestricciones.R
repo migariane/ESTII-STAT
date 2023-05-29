278f58f0edee97a669b4f32631ca3b36e8eef8ea
migariane <watzilei@hotmail.com>
2023-05-29 13:09:55 +0200

    update

--- OptimizacionSinRestricciones.R
+++ OptimizacionSinRestricciones.R
@@ -1,16 +1,18 @@
-# Optimizacion sin restricciones bivariada
-f <- function(x) ((x[1])^2 + (x[2])^2)
+# Optimizacion sin restricciones bivariada: Ejemplo clase
+f <- function(x) ((x[1])^2 - (x[2])^2)
 r <- optim(c(1, 1), f);r
 # Optimo (0,0)
 
 # Derivadas en R
 f <- expression((x)^2 - (y)^2)
 # Hessian
-fprimaxy <- D(f,"x")
+fprimax <- D(f,"x")
 fprimax
-fprimaxy <- D(f,"y")
-fprimaxy
+fprimay <- D(f,"y")
+fprimay
 fprimaxy <- D(f,"yx")
+fprimaxy
+fprimaxy <- D(f,"xy")
 fprimaxy
 
 # Grafica de la funcion
@@ -26,8 +28,13 @@ plot_ly(x = input_1, y = input_2, z = z) %>% add_surface()
 
 
 # Otro ejemplo
+# Minimizacion
 f <- function(x) (x[1]^3 + 3*x[1]*x[2]^2 -15*x[1] -12*x[2])
-r <- optim(c(5, 5), f);r
+r <- optim(c(1, 1), f, hessian=T);r
+# Maximizacion (-)
+f <- function(x) -(x[1]^3 + 3*x[1]*x[2]^2 -15*x[1] -12*x[2])
+r <- optim(c(1, 1), f, hessian=T);r
+
 # Optimo (2,1): Minimo
 # Derivadas parciales en R y Hessian
 f <- expression(x^3 + 3*x*y^2 -15*x -12*y)
@@ -39,21 +46,23 @@ fprimay <- D(f,"y")
 fprimay
 
 # Hessian
+# Partial derivatives
 f2x <- expression(3 * x^2 + 3 * y^2 - 15)
-f2primax <- D(f2x,"x")
-f2primax
+f2xprimax <- D(f2x,"x")
+f2xprimax
 
 f2y <- expression(6 * x*y - 12)
 f2yprimax <- D(f2y,"y")
 f2yprimax
 
-f2xy <- expression(3 * x^2 + 3 * y^2 - 15)
-f2xyprimax <- D(f2x,"y")
-f2xyprimax
+#Partial derivatives xy
+f2x <- expression(3 * x^2 + 3 * y^2 - 15)
+f2xprimax <- D(f2xy,"y")
+f2xprimax
 
-f2yx <- expression(6 * x*y - 12)
-f2yxprimax <- D(f2xy,"x")
-f2yxprimax
+f2y <- expression(6 * x*y - 12)
+f2yprimax <- D(f2y,"x")
+f2yprimax
 
 my_function <- function(x,y) {
   final_value = x^3 + 3*x*y^2 -15*x -12*y
