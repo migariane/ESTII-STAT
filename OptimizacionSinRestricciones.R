e74e3ede061cb38fa71b1e73fa767b1836f59790
migariane <watzilei@hotmail.com>
2023-05-30 18:50:46 +0200

    update

--- OptimizacionSinRestricciones.R
+++ OptimizacionSinRestricciones.R
@@ -1,6 +1,6 @@
 # Optimizacion sin restricciones bivariada: Ejemplo clase
 f <- function(x) ((x[1])^2 - (x[2])^2)
-r <- optim(c(1, 1), f);r
+r <- optim(c(2, 2), f, hessian=T);r
 # Optimo (0,0)
 
 # Derivadas en R
@@ -10,12 +10,13 @@ fprimax <- D(f,"x")
 fprimax
 fprimay <- D(f,"y")
 fprimay
-fprimaxy <- D(f,"yx")
-fprimaxy
+fprimayx <- D(f,"yx")
+fprimayx
 fprimaxy <- D(f,"xy")
 fprimaxy
 
 # Grafica de la funcion
+# install.packages("plotly")
 library(plotly)
 my_function <- function(x,y) {
   final_value = x^2 - y^2
@@ -23,6 +24,7 @@ my_function <- function(x,y) {
 
 input_1 <- seq(-1.5, 1.5,0.1)
 input_2 <- seq(-1.5, 1.5,0.1)
+
 z <- outer(input_1, input_2, my_function)
 plot_ly(x = input_1, y = input_2, z = z) %>% add_surface()
 
@@ -72,3 +74,22 @@ input_1 <- seq(-3, 3,0.1)
 input_2 <- seq(-3, 3,0.1)
 z <- outer(input_1, input_2, my_function)
 plot_ly(x = input_1, y = input_2, z = z) %>% add_surface()
+
+# Optimizacion con restriciones (ejemplo clase)
+
+install.packages("lpSolve")
+library(lpSolve)
+
+# Objective function
+objective.in <- c(3, 6)
+# Variables and restrictions
+const.mat <- matrix(c(20, 50, 4, 3), nrow=2,
+                    byrow=TRUE)
+const.rhs <- c(3300, 380)
+const.dir <- c("<=", "<=")
+
+# Optimization function
+optimum <- lp(direction="max", objective.in, const.mat,
+              const.dir, const.rhs)
+optimum$solution 
+optimum$objval
