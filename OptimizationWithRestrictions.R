a5c0fac62d3762db32b9eba016ab31cb7dac3cab
migariane <watzilei@hotmail.com>
2023-06-02 10:39:39 +0200

    update

--- OptimizationWithRestrictions.R
+++ OptimizationWithRestrictions.R
@@ -2,6 +2,8 @@
 # install.packages("lpSolve")
 library(lpSolve)
 
+# Primer ejemplo: solucion en vertice
+
 # Objective function
 objective.in <- c(3, 6)
 
@@ -18,5 +20,21 @@ optimum$solution
 optimum$objval
 print(optimum)
 
+# Segundo ejemplo: solucion en arista (in R only one point)
 
+# Objective function
+objective.in <- c(400, 500)
+
+# Variables and restrictions
+const.mat <- matrix(c(4000, 5000, 4, 3, 3, 2, 2,1), nrow=4,
+                    byrow=TRUE)
+const.rhs <- c(32000, 24, 20, 16)
+const.dir <- c("<=", "<=")
+
+# Optimization function
+optimum <- lp(direction="max", objective.in, const.mat,
+              const.dir, const.rhs)
+optimum$solution 
+optimum$objval
+print(optimum)
 
